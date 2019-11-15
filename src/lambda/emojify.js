80d4e92eaef9f41654943e918d992f8400c3c73e
a-sherwood <asherwood@deseretdigital.com>
2019-11-15 15:03:15 -0700

    Remove comments.

--- src/lambda/emojify.js
+++ src/lambda/emojify.js
@@ -1,9 +1,5 @@
 let translate = require("moji-translate");
-// let fetch = require("node-fetch");
 let qs = require("qs");
-
-// console.log(translate.getAllEmojiForWord(':eyes:'));
-// console.log(translate.translate("the house is on fire and the cat is eating the cake"));
 
 exports.handler = function(event, context, callback) {
   callback(null, {
@@ -14,10 +10,4 @@ exports.handler = function(event, context, callback) {
     }),
     headers: { "Content-Type": "application/json" }
   });
-  // let body = qs.parse(event.body);
-  // fetch(body.response_url, {
-  //   method: "post",
-  //   body: translate.translate(body.text),
-  //   headers: { "Content-Type": "text/plain" }
-  // })
 };
