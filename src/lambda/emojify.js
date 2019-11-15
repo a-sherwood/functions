7632cdd0f8dbb70a44ead9722f66dbeab49dcbd1
a-sherwood <asherwood@deseretdigital.com>
2019-11-15 14:39:58 -0700

    let...

--- src/lambda/emojify.js
+++ src/lambda/emojify.js
@@ -1,4 +1,4 @@
-translate = require("moji-translate");
+let translate = require("moji-translate");
 
 // console.log(translate.getAllEmojiForWord(':eyes:'));
 // console.log(translate.translate("the house is on fire and the cat is eating the cake"));
