c55cd4fa18a343f830a8cd411d7f817a201176eb
a-sherwood <asherwood@deseretdigital.com>
2019-11-15 14:52:13 -0700

    Add qs

--- src/lambda/emojify.js
+++ src/lambda/emojify.js
@@ -1,4 +1,6 @@
 let translate = require("moji-translate");
+// let fetch = require("node-fetch");
+let qs = require("qs");
 
 // console.log(translate.getAllEmojiForWord(':eyes:'));
 // console.log(translate.translate("the house is on fire and the cat is eating the cake"));
@@ -7,7 +9,14 @@ exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
     body: translate.translate(
-      "the house is on fire and the cat is eating the cake!"
+      qs.parse(body).text
     )
   });
+  // let body = qs.parse(event.body);
+  // fetch(body.response_url, {
+  //   method: "post",
+  //   body: translate.translate(body.text),
+  //   headers: { "Content-Type": "text/plain" }
+  // })
+  
 };
